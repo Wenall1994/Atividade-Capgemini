6be24d40abbbed76b43615e735ac290c15d1876f
Wenall1994 <62037056+Wenall1994@users.noreply.github.com>
2022-02-20 10:24:50 -0300

    commit 2
    Atividade 2

--- src/main/java/Atividade2.java
+++ src/main/java/Atividade2.java
@@ -1,5 +1,23 @@
 public class Atividade2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String palavra = "ovo";
+        imprimeAnagramas("", palavra);
 
     }
+
+    public static void imprimeAnagramas(String prefix, String word) {
+        if (word.length() <= 1) {
+            System.out.println(prefix + word);
+        } else {
+            for (int i = 0; i < word.length(); i++) {
+                String cur = word.substring(i, i + 1);
+                String before = word.substring(0, i);
+                String after = word.substring(i + 1);
+                imprimeAnagramas(prefix + cur, before + after);
+            }
+        }
+    }
+
+
 }
+
